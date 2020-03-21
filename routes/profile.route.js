1574b4159b9eaa4c4933b70b07b32377396d0255
Humberto Martins <humberto@consilio.com.br>
2020-03-21 10:56:32 -0300

    update: remve architecture mvc

--- routes/profile.route.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-import profileController from "../controllers/profile.controller";
-
-export default function(app) {
-  app.route("/profile").get(profileController.profileVerify);
-}
--- routes/webhook.route.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-import webhookController from "../controllers/webhook.controller";
-
-export default function(app) {
-  app.route("/webhook").get(webhookController.verifyWebhook);
-  app.route("/webhook").post(webhookController.messageHandler);
-}
